569472155d2403ec8aea66fac5f9449d1e341485
Rahul Sharma <40212890+257277@users.noreply.github.com>
2023-05-13 18:12:24 +0530

    Redoy backend changes link in backend and frontend

--- Backend/config/facebook-oauth.js
+++ Backend/config/facebook-oauth.js
@@ -2,7 +2,7 @@ const FacebookStrategy = require("passport-facebook").Strategy;
 const passport2 = require("passport");
 const { UserModel } = require("../models/user.schema");
 const { v4: uuidv4 } = require("uuid");
-const {redisclient}=require("./redis")
+const { redisclient } = require("./redis")
 const { JsonWebTokenError } = require("jsonwebtoken");
 require("dotenv").config();
 passport2.use(
@@ -10,11 +10,11 @@ passport2.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "http://localhost:8080/facebook/auth/facebook/callback",
+      callbackURL: "https://zany-gray-clownfish-shoe.cyclic.app/facebook/auth/facebook/callback",
       profileFields: ["id", "displayName", "photos", "email"],
     },
     async function (accessToken, refreshToken, profile, cb) {
-      await redisclient.SET("tokens", JSON.stringify({"token":accessToken}));
+      await redisclient.SET("tokens", JSON.stringify({ "token": accessToken }));
       let email = profile._json.email;
       let udata = await UserModel.findOne({ email });
       if (udata) {
